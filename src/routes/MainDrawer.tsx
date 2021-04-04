50f57ba3e79be86a9347a5e7f0b94fddd27a49c8
Luiz Pires <lpiresjunior@gmail.com>
2021-04-03 23:01:23 -0300

    drawer navigator screen options

--- src/routes/MainDrawer.tsx
+++ src/routes/MainDrawer.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import {useColorScheme} from 'react-native';
+
+import themes from '../themes';
+import CustomDrawer from '../components/CustomDrawer';
 
 import WallScreen from '../screens/WallScreen';
-
-import CustomDrawer from '../components/CustomDrawer';
 
 const Drawer = createDrawerNavigator();
 
 const MainDrawer: React.FC = () => {
+  const deviceTheme = useColorScheme();
+  const theme = deviceTheme ? themes[deviceTheme] : themes.dark;
+
   return (
-    <Drawer.Navigator drawerContent={props => <CustomDrawer {...props} />}>
-      <Drawer.Screen name="WallScreen" component={WallScreen} />
+    <Drawer.Navigator
+      screenOptions={{
+        headerShown: true,
+        headerStyle: {
+          backgroundColor: theme.background,
+          elevation: 0,
+          shadowOpacity: 0,
+        },
+        headerTintColor: theme.text,
+      }}
+      drawerContent={props => <CustomDrawer {...props} />}>
+      <Drawer.Screen
+        name="WallScreen"
+        component={WallScreen}
+        options={{title: 'Mural de Avisos'}}
+      />
     </Drawer.Navigator>
   );
 };
