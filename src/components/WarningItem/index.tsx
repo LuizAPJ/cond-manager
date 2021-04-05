617507f0e725d724e694fc607163cb866dcfc9fd
Luiz Pires <lpiresjunior@gmail.com>
2021-04-05 16:03:01 -0300

    warning item styles

--- src/components/WarningItem/index.tsx
+++ src/components/WarningItem/index.tsx
@@ -1,5 +1,5 @@
-import React, {useCallback} from 'react';
-import {Linking, useColorScheme} from 'react-native';
+import React from 'react';
+import {useColorScheme} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import themes from '../../themes';
@@ -15,19 +15,28 @@ const WarningItem: React.FC<WarningItemProps> = ({data}) => {
   const deviceTheme = useColorScheme();
   const theme = deviceTheme ? themes[deviceTheme] : themes.dark;
 
-  const handleClick = useCallback(async () => {
-    const supported = await Linking.canOpenURL(data.photos[1]);
-
-    if (supported) {
-      await Linking.openURL(data.photos[1]);
-    }
-  }, [data]);
-
   return (
-    <S.Button onPress={handleClick}>
-      <Icon name="file-text" size={30} color={theme.purple} />
+    <S.Container>
+      <S.Date>{data.datecreated}</S.Date>
       <S.Title>{data.title}</S.Title>
-    </S.Button>
+
+      <S.StatusContainer>
+        <Icon name="inbox" size={24} color={theme.purple} />
+        <S.StatusText>
+          {data.status === 'RESOLVED' ? 'Resolvido' : 'Ocorrência em análise'}
+        </S.StatusText>
+      </S.StatusContainer>
+
+      {data.photos.length > 0 && (
+        <S.PhotosContainer>
+          {data.photos.map((item, index) => (
+            <S.PhotoButton key={index} onPress={() => {}}>
+              <S.PhotoImage source={{uri: item}} resizeMode="cover" />
+            </S.PhotoButton>
+          ))}
+        </S.PhotosContainer>
+      )}
+    </S.Container>
   );
 };
 
--- src/components/WarningItem/style.ts
+++ src/components/WarningItem/style.ts
@@ -1,19 +1,43 @@
 import styled from 'styled-components/native';
 
 export default {
-  Button: styled.TouchableOpacity`
+  Container: styled.TouchableOpacity`
     background-color: ${props => props.theme.lighterBackground};
     border-width: 2px;
     border-color: ${props => props.theme.purple};
     border-radius: 15px;
     padding: 15px;
     margin-bottom: 10px;
-    flex-direction: row;
-    align-items: center;
+  `,
+  Date: styled.Text`
+    font-size: 14px;
+    font-weight: bold;
+    color: ${props => props.theme.grayText};
+    margin-bottom: 10px;
   `,
   Title: styled.Text`
     font-size: 15px;
     color: ${props => props.theme.text};
+  `,
+  StatusContainer: styled.View`
+    flex-direction: row;
+    align-items: center;
+    margin: 10px 0;
+  `,
+  StatusText: styled.Text`
+    font-size: 14px;
+    color: ${props => props.theme.grayText};
     margin-left: 10px;
+  `,
+  PhotosContainer: styled.View`
+    flex-direction: row;
+  `,
+  PhotoButton: styled.TouchableOpacity`
+    margin-right: 10px;
+  `,
+  PhotoImage: styled.Image`
+    width: 50px;
+    height: 50px;
+    border-radius: 10px;
   `,
 };
