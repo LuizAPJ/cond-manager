a290e5d90d22bf21ab75037335b4fbcc2bab7b15
Luiz Pires <lpiresjunior@gmail.com>
2021-04-08 04:22:21 -0300

    reservation screen structure

--- src/screens/ReservationScreen/index.tsx
+++ src/screens/ReservationScreen/index.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
-import {useColorScheme} from 'react-native';
+import React, {useCallback, useEffect, useState} from 'react';
+import {Alert, useColorScheme} from 'react-native';
 
+import api from '../../services/api';
 import themes from '../../themes';
+import ReservationItem from '../../components/ReservationItem';
+import IReservationItem from '../../interfaces/ReservationItem';
 
 import S from './style';
 
@@ -9,9 +12,46 @@ const ReservationScreen: React.FC = () => {
   const deviceTheme = useColorScheme();
   const theme = deviceTheme ? themes[deviceTheme] : themes.dark;
 
+  const [loading, setLoading] = useState(true);
+  const [list, setList] = useState<IReservationItem[]>([]);
+
+  const getReservations = useCallback(async () => {
+    setLoading(true);
+    const {data: response} = await api.get('/reservations');
+    setLoading(false);
+
+    if (!response.error) {
+      setList(response.list);
+    } else {
+      Alert.alert('Erro!', `${response.error}`);
+    }
+  }, []);
+
+  useEffect(() => {
+    getReservations();
+  }, [getReservations]);
+
   return (
     <S.Container>
-      <S.Title>ReservationScreen</S.Title>
+      <S.Scroller>
+        <S.Button>
+          <S.ButtonText>Minhas Reservas</S.ButtonText>
+        </S.Button>
+
+        <S.Title>Selecione uma Aŕea</S.Title>
+
+        {loading && <S.LoadingIcon size="large" color={theme.purple} />}
+
+        {!loading && list.length === 0 && (
+          <S.NoListContainer>
+            <S.NoListText>Não há áreas disponíveis.</S.NoListText>
+          </S.NoListContainer>
+        )}
+
+        {list.map((item, index) => (
+          <ReservationItem key={index} data={item} />
+        ))}
+      </S.Scroller>
     </S.Container>
   );
 };
--- src/screens/ReservationScreen/style.ts
+++ src/screens/ReservationScreen/style.ts
@@ -3,6 +3,37 @@ import styled from 'styled-components/native';
 export default {
   Container: styled.SafeAreaView`
     flex: 1;
+    background-color: ${props => props.theme.background};
   `,
-  Title: styled.Text``,
+  Scroller: styled.ScrollView`
+    flex: 1px;
+    padding: 20px;
+  `,
+  LoadingIcon: styled.ActivityIndicator``,
+  Button: styled.TouchableOpacity`
+    background-color: ${props => props.theme.purple};
+    justify-content: center;
+    align-items: center;
+    padding: 12px;
+    border-radius: 10px;
+  `,
+  ButtonText: styled.Text`
+    font-size: 15px;
+    font-weight: bold;
+    color: ${props => props.theme.buttonText};
+  `,
+  Title: styled.Text`
+    font-size: 17px;
+    color: ${props => props.theme.text};
+    margin: 10px 0;
+  `,
+  NoListContainer: styled.View`
+    justify-content: center;
+    align-items: center;
+    padding: 30px;
+  `,
+  NoListText: styled.Text`
+    font-size: 15px;
+    color: ${props => props.theme.text};
+  `,
 };
