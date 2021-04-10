4fb76798f5db738465418baae1a2f578b9b2d7b9
Luiz Pires <lpiresjunior@gmail.com>
2021-04-10 06:01:52 -0300

    LostItem functions

--- src/components/LostItem/index.tsx
+++ src/components/LostItem/index.tsx
@@ -22,6 +22,26 @@ const LostItem: React.FC<LostItemProps> = ({
   const deviceTheme = useColorScheme();
   const theme = deviceTheme ? themes[deviceTheme] : themes.dark;
 
+  const handleSetRecovered = useCallback(async () => {
+    const {data: response} = await api.put(`/foundandlost/${data.id}`, {
+      status: 'recovered',
+    });
+
+    if (!response.error) {
+      refreshFunction();
+      Alert.alert('Concluído', 'Pegue seu item perdido na portaria!');
+    } else {
+      Alert.alert('Erro!', `${response.error}`);
+    }
+  }, [data.id, refreshFunction]);
+
+  const handleIsMine = useCallback(() => {
+    Alert.alert('Confirmação', 'Tem certeza que este item é seu?', [
+      {text: 'Sim, é meu', onPress: handleSetRecovered},
+      {text: 'Cancelar', onPress: () => {}, style: 'cancel'},
+    ]);
+  }, [handleSetRecovered]);
+
   return (
     <S.Container>
       <S.Photo source={{uri: data.photo}} resizeMode="cover" />
@@ -34,7 +54,7 @@ const LostItem: React.FC<LostItemProps> = ({
       <S.InfoText>{data.datecreated}</S.InfoText>
 
       {showButton && (
-        <S.MineButton onPress={() => {}}>
+        <S.MineButton onPress={handleIsMine}>
           <Icon name="hand-pointer-o" size={24} color={theme.buttonText} />
           <S.MineButtonText>É meu</S.MineButtonText>
         </S.MineButton>
