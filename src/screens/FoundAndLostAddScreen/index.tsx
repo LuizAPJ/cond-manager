03bebeee358b8a6bbb5d123fd0d91ee0c826209f
Luiz Pires <lpiresjunior@gmail.com>
2021-04-10 07:51:49 -0300

    FoundAndLostAddScreen save item

--- src/screens/FoundAndLostAddScreen/index.tsx
+++ src/screens/FoundAndLostAddScreen/index.tsx
@@ -1,11 +1,9 @@
 import React, {useCallback, useState} from 'react';
-import {useColorScheme} from 'react-native';
-// import {useNavigation} from '@react-navigation/native';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
+import {Alert, useColorScheme} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 import {launchCamera, ImagePickerResponse} from 'react-native-image-picker';
-
+import api from '../../services/api';
 import themes from '../../themes';
-// import DocItem from '../../components/DocItem';
 
 import S from './style';
 
@@ -13,7 +11,7 @@ const FoundAndLostAddScreen: React.FC = () => {
   const deviceTheme = useColorScheme();
   const theme = deviceTheme ? themes[deviceTheme] : themes.dark;
 
-  // const navigation = useNavigation();
+  const navigation = useNavigation();
 
   const [photo, setPhoto] = useState<ImagePickerResponse>(
     {} as ImagePickerResponse,
@@ -26,6 +24,7 @@ const FoundAndLostAddScreen: React.FC = () => {
       {
         mediaType: 'photo',
         maxWidth: 1280,
+        maxHeight: 1280,
       },
       response => {
         if (!response.didCancel) {
@@ -34,6 +33,36 @@ const FoundAndLostAddScreen: React.FC = () => {
       },
     );
   }, []);
+
+  const handleSave = useCallback(async () => {
+    if (photo && where && photo.uri) {
+      let formData = new FormData();
+      formData.append('description', description);
+      formData.append('where', where);
+      formData.append('photo', {
+        uri: photo.uri,
+        type: photo.type,
+        name: photo.fileName,
+      });
+
+      const {data: response} = await api.post('/foundandlost', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      if (!response.error) {
+        setPhoto({});
+        setDescription('');
+        setWhere('');
+        navigation.navigate('FoundAndLostScreen');
+      } else {
+        Alert.alert('Erro!', `${response.error}`);
+      }
+    } else {
+      Alert.alert('Erro!', 'Preencha todos os campos e tire uma foto.');
+    }
+  }, [description, navigation, photo, where]);
 
   return (
     <S.Container>
@@ -71,7 +100,7 @@ const FoundAndLostAddScreen: React.FC = () => {
           placeholderTextColor={theme.grayText}
         />
 
-        <S.Button>
+        <S.Button onPress={handleSave}>
           <S.ButtonText>SALVAR</S.ButtonText>
         </S.Button>
       </S.Scroller>
