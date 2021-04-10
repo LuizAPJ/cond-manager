f9f8de981f36757dc92d60238496019ef77bba1a
Luiz Pires <lpiresjunior@gmail.com>
2021-04-10 05:04:59 -0300

    FoundAndLostScreen add button

--- src/screens/FoundAndLostScreen/index.tsx
+++ src/screens/FoundAndLostScreen/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useColorScheme} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import themes from '../../themes';
 
@@ -12,6 +13,16 @@ const FoundAndLostScreen: React.FC = () => {
   const theme = deviceTheme ? themes[deviceTheme] : themes.dark;
 
   const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <S.AddButton>
+          <Icon name="plus" size={24} color={theme.text} />
+        </S.AddButton>
+      ),
+    });
+  }, [navigation, theme]);
 
   return (
     <S.Container>
--- src/screens/FoundAndLostScreen/style.ts
+++ src/screens/FoundAndLostScreen/style.ts
@@ -3,6 +3,10 @@ import styled from 'styled-components/native';
 export default {
   Container: styled.SafeAreaView`
     flex: 1;
+    background-color: ${props => props.theme.background};
+  `,
+  AddButton: styled.TouchableOpacity`
+    margin-right: 15px;
   `,
   Title: styled.Text``,
 };
